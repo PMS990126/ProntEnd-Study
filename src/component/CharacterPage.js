72a51394e2a3878bba4192340dec0ed519f7c526
ruri <sub08041@naver.com>
2024-02-01 15:25:43 +0900

    .

--- src/component/CharacterPage.js
+++ src/component/CharacterPage.js
@@ -4,13 +4,10 @@ import styled from 'styled-components';
 import loading from '../picture/주황버섯점프.gif';
 
 export default function CharacterPage() {
-    // const { ocid } = useParams(); //URL의 파리미터에서 ocid(식별자)를 가져옴
+    const { ocid } = useParams(); //URL의 파리미터에서 ocid(식별자)를 가져옴
     const [characterData, setCharacterData] = useState(null); //캐릭터의 데이터를 저장하는 상태
     const [popularityData, setPopularityData] = useState(null); //캐릭터의 인기도를 저장하는 상태
     const [isLoading, setIsLoading] = useState(true); // 로딩 상태를 나타내는 상태 변수
-
-    const urlParams = new URL(location.href).searchParams;
-    const ocid = urlParams.get('ocid');
 
     const today = new Date();
     const year = today.getFullYear(); // 년도
@@ -21,6 +18,7 @@ export default function CharacterPage() {
     useEffect(() => {
         fetchCharacterData(ocid); //식별자를 이용해 캐릭터 데이터 불러오기
         fetchCharacterPopularityData(ocid); //식별자를 이용해 캐릭터 인기도 불러오기
+        fetchCharacterCashitemEquipmentData(ocid);
     }, [ocid]);
 
     useEffect(() => {
@@ -32,7 +30,7 @@ export default function CharacterPage() {
 
     const fetchCharacterData = (ocid) => {
         const maple_api = process.env.REACT_APP_NEXON_OPEN_API;
-        fetch(`https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${ocid}&date=${usingday}`, {
+        fetch(`https://open.api.nexon.com/maplestory/v1/character/basic?ocid=431628398b08ed28405e09b986f97c2f&date=2024-01-31`, {
             method: 'GET',
             headers: {
                 accept: 'application/json',
@@ -49,7 +47,7 @@ export default function CharacterPage() {
 
     const fetchCharacterPopularityData = (ocid) => {
         const maple_api = process.env.REACT_APP_NEXON_OPEN_API;
-        fetch(`https://open.api.nexon.com/maplestory/v1/character/popularity?ocid=${ocid}&date=${usingday}`, {
+        fetch(`https://open.api.nexon.com/maplestory/v1/character/popularity?ocid=431628398b08ed28405e09b986f97c2f&date=2024-01-31`, {
             method: 'GET',
             headers: {
                 accept: 'application/json',
@@ -64,7 +62,25 @@ export default function CharacterPage() {
             .catch((error) => console.error('Error:', error));
     };
 
-    if (isLoading || !characterData) {
+    const fetchCharacterCashitemEquipmentData = (ocid) => {
+        const maple_api = process.env.REACT_APP_NEXON_OPEN_API;
+        fetch(`https://open.api.nexon.com/maplestory/v1/character/cashitem-equipment?ocid=431628398b08ed28405e09b986f97c2f&date=2024-01-31
+`, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                'x-nxopen-api-key': maple_api,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setPopularityData(data); //불러온 데이터를 상태에 저장
+                setIsLoading(false); // 데이터를 불러온 후 로딩 상태를 false로 설정
+            })
+            .catch((error) => console.error('Error:', error));
+    };
+
+    if (isLoading || !characterData||!popularityData) {
         return (
             <TitleText>
                 <LoadingImg src={loading} alt="loading" />
@@ -76,7 +92,8 @@ export default function CharacterPage() {
     return (
         <CharacterContainer>
             <CashItemContainer>
-                <div>캐시아이템 api사용 구현 예정</div>
+                <CashItemContainer>모자</CashItemContainer>
+                <CashItemContainer>모자</CashItemContainer>
             </CashItemContainer>
 
             <CharacterImageContainer>
@@ -86,27 +103,29 @@ export default function CharacterPage() {
 
             <CharacterInfoContainer>
                 <CharacterName>
-                    {characterData.character_name} Guild: {characterData.character_guild_name}
+                    {characterData.character_name} | {characterData.world_name	} 
                 </CharacterName>
                 <CharacterDetail>
-                    Lv: {characterData.character_level} | Class: {characterData.character_class}
+                    Lv: {characterData.character_level} | {characterData.character_class} | 인기도 {popularityData.popularity} | {characterData.character_guild_name}
                 </CharacterDetail>
-
-                <CharacterDetail>EXP: {characterData.character_exp}</CharacterDetail>
             </CharacterInfoContainer>
         </CharacterContainer>
     );
 }
 
 const CharacterContainer = styled.div`
-    padding-top: 60px;
-    width: 20vw;
+
+    padding: 80px 80px;
+    width: 100%;
     display: flex;
     flex-direction: row;
     flex-grow: 1;
 `;
 
-const CharacterInfoContainer = styled.div``;
+const CharacterInfoContainer = styled.div`
+background-color: rgba(33, 34, 39);
+flex-dirction:column;
+`;
 
 const CharacterImageContainer = styled.div`
     flex-direction: column;
@@ -115,18 +134,19 @@ const CharacterImageContainer = styled.div`
 const CashItemContainer = styled.div`
     padding: 5px;
     width: 100px;
-    height: 200;
+    height: 200px;
     display: flex;
 `;
 
 const CharacterImage = styled.img`
-    width: 100px;
-    height: 200px;
+    width: 150px;
+    height: 150px;
 `;
 
 const CharacterName = styled.div`
     font-size: 20px;
     flex-direction: row;
+    display:flex;
 `;
 
 const TitleText = styled.div`
@@ -147,5 +167,7 @@ const LoadingImg = styled.img`
 `;
 
 const CharacterDetail = styled.div`
-    /* 원하는 스타일을 적용합니다. */
+margin : 15px 0;
+    width:100%;
+    height:100%;
 `;
